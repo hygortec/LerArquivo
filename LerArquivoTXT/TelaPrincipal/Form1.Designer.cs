b08ff2166d98045405d977ca1199e6bbfee745d7
hygortec <hygor.tec@gmail.com>
2016-03-29 09:25:10 -0300

    Inicial

---
+++ LerArquivoTXT/TelaPrincipal/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace TelaPrincipal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.tbCaminho = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(197, 22);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 0;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.acaoBotaoBuscar);
+            // 
+            // tbCaminho
+            // 
+            this.tbCaminho.Location = new System.Drawing.Point(12, 24);
+            this.tbCaminho.Name = "tbCaminho";
+            this.tbCaminho.Size = new System.Drawing.Size(179, 20);
+            this.tbCaminho.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.tbCaminho);
+            this.Controls.Add(this.btnBuscar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox tbCaminho;
+    }
+}
+
---
+++ LerArquivoTXT/TelaPrincipal/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TelaPrincipal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void acaoBotaoBuscar(object sender, EventArgs e)
+        {
+            this.openFileDialog.Multiselect = false;
+            this.openFileDialog.Title = "Selecionar AFD";
+            //openFileDialog.InitialDirectory = @"C:\Users\macoratti\Pictures";
+            openFileDialog.Filter = "ARQUIVOS (*.TXT)|*.TXT|" + "All files (*.*)|*.*";
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.ReadOnlyChecked = true;
+            openFileDialog.ShowReadOnly = true;
+
+            DialogResult caixaDialogo = this.openFileDialog.ShowDialog();
+
+            //MessageBox.Show(Convert.ToString(dr));
+            MessageBox.Show(openFileDialog.FileName);
+        }
+    }
+}

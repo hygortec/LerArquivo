6d13378a7ad6eed80b8ebcfc904b24b8565ba53d
hygortec <hygor.tec@gmail.com>
2016-03-31 14:49:23 -0300

    Inclusão função de conexão com o banco de dados

--- LerArquivoTXT/TelaPrincipal/Banco.cs
+++ LerArquivoTXT/TelaPrincipal/Banco.cs
@@ -5,26 +5,56 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace TelaPrincipal
 {
     class Banco
     {
-        SqlConnection conexao =  null;  
+        SqlConnection conexaoSQL =  null;
+        MySqlConnection conexaoMysql = null;
+
+        private string servidor;
+        private string banco;
+        private string usuario;
+        private string senha;
+
 
         public Banco(string servidor, string banco, string usuario, string senha)
         {
+            this.servidor = servidor;
+            this.banco = banco;
+            this.usuario = usuario;
+            this.senha = senha;
+        }
+        public void conectaSQL()
+        {
             try
             {
-                conexao = new SqlConnection("Data Source=" + servidor + "; Initial Catalog=" + banco + "; User id=" + usuario + "; Password=" + senha + ";");
-                conexao.Open();
-                
+                conexaoSQL = new SqlConnection("Data Source=" + servidor + "; Initial Catalog=" + banco + "; User id=" + usuario + "; Password=" + senha + ";");
+                conexaoSQL.Open();
+
                 MessageBox.Show("Conexão ok");
             }
-            catch {
+            catch
+            {
                 MessageBox.Show("Erro ao conectar no banco de dados.");
-            }          
+            }
 
+        }
+        public void conectaMysql(string servidor, string banco, string usuario, string senha)
+        {
+            try
+            {
+                conexaoMysql = new MySqlConnection("Data Source=" + servidor + "; Initial Catalog=" + banco + "; User id=" + usuario + "; Password=" + senha + ";");
+                conexaoMysql.Open();
+
+                MessageBox.Show("Conexão ok");
+            }
+            catch
+            {
+                MessageBox.Show("Erro ao conectar no banco de dados.");
+            }
         }
 
          
